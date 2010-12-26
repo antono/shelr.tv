deaed549157e2e2c4da4cf69666c91810331744f
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-12-26 07:22:37 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shelltube::Application.config.secret_token = '42ef6de9eac6436c46f740a62a19b29f93c30827078bda5f6994e032a1748703ae16b6548c7aa1add9d72d6ce6c0fd58f36c1520fe65318e99a49844c157aaf0'
