45f5dfb677ddad16eb67263bfb59882fa7a141c2
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-12-30 01:41:36 +0200

    Dropped Oauth. Will use simple md5 auth

--- Gemfile
+++ Gemfile
@@ -3,28 +3,8 @@ source 'http://rubygems.org'
 gem 'rails'
 gem 'haml'
 gem 'omniauth'
-gem 'oauth-plugin', ">=0.4.0.pre1"
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
-# Use unicorn as the web server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
-# gem 'ruby-debug'
-# gem 'ruby-debug19'
-
-# Bundle the extra gems:
-# gem 'bj'
-# gem 'nokogiri'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
-# gem 'aws-s3', :require => 'aws/s3'
-
-# Bundle gems for the local environment. Make sure to
-# put test-only gems in this group so their generators
-# and rake tasks are available in development mode:
 group :development, :test do
   gem 'rspec'
   gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,76 +1,13 @@
 Shelltube::Application.routes.draw do
 
-  match '/oauth/test_request',  :to => 'oauth#test_request',  :as => :test_request
-  match '/oauth/token',         :to => 'oauth#token',         :as => :token
-  match '/oauth/access_token',  :to => 'oauth#access_token',  :as => :access_token
-  match '/oauth/request_token', :to => 'oauth#request_token', :as => :request_token
-  match '/oauth/authorize',     :to => 'oauth#authorize',     :as => :authorize
-  match '/oauth',               :to => 'oauth#index',         :as => :oauth
-
-  match '/login',  :to => 'authentications#login',  :as => :login
-  match '/logout', :to => 'authentications#logout', :as => :logout
-
   resources :users
   resources :records
-  resources :oauth_clients
   resources :authentications
 
   match '/auth/:provider/callback' => 'authentications#create'
+  match '/login',  :to => 'authentications#login',  :as => :login
+  match '/logout', :to => 'authentications#logout', :as => :logout
+
   root :to => "records#index"
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
