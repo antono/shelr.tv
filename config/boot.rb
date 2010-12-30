02cdd148210db0c3184f3b333bda9b9cac9cb435
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-12-30 08:49:24 +0200

    Bundler.setup back

--- config/boot.rb
+++ config/boot.rb
@@ -5,7 +5,7 @@ gemfile = File.expand_path('../../Gemfile', __FILE__)
 begin
   ENV['BUNDLE_GEMFILE'] = gemfile
   require 'bundler'
-  #Bundler.setup
+  Bundler.setup
 rescue Bundler::GemNotFound => e
   STDERR.puts e.message
   STDERR.puts "Try running `bundle install`."
