dc0769b3be6b119d2ef84c1010c46073a6d4871a
Antono Vasiljev <antono.vasiljev@gmail.com>
2010-12-30 10:21:39 +0200

    Login required for some actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  before_filter :login_required, except: [:show, :login, :authenticate]
 
   def show
     @user = User.where(nickname: params[:id]).first
