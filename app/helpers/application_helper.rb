7b23e7bdde491646872eae199232ca9def408230
Antono Vasiljev <self@antono.info>
2012-02-22 17:11:26 +0300

    Counters for...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def controller_and_action_class_names
     [controller_name, '-controller', ' ', action_name, '-action'].join
   end
+
+  def active_if(regex)
+    'active' if request.env['PATH_INFO'] =~ regex
+  end
 end
