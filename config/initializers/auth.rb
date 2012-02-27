fbb34e555ab69c0a7be88c4e82fba65b182f7861
Antono Vasiljev <self@antono.info>
2012-02-27 15:45:42 +0300

    Disable some warningx

--- config/initializers/auth.rb
+++ config/initializers/auth.rb
@@ -1,13 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  
   provider :developer unless Rails.env.production?
+  
   provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'],
         :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
   provider :twitter, 'LviFieaqd8pUfcfa8LxcNg', 'oSxUo8k6K9W47ku9l6x0F2m3O2D3u8FbrrT7ym1fI'
   provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], {
-    access_type: 'online',
-    approval_prompt: ''
+    :access_type => 'online',
+    :approval_prompt => ''
   }
 
   # provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
 end
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,1 @@
 # Be sure to restart your server when you modify this file.
-
-Mime::Type.register "application/rss+xml", :rss
-Mime::Type.register "application/atom+xml", :atom
