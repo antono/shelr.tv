2e205454859c97c94f0a4de984e4a89cda81d4cb
Antono Vasiljev <self@antono.info>
2012-04-07 13:38:26 +0300

    add spork for ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
 require 'rubygems'
+require File.expand_path("../../config/environment", __FILE__)
 require 'spork'
 require 'capybara/rails'
 require 'capybara/rspec'
