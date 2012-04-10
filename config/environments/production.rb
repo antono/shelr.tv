8e7bc3c430accb9fa912d08dafa6619361722c65
Max Gonzih <gonzih@gmail.com>
2012-04-10 13:49:53 +0300

    Generate digests for assets URLs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Shelr::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
 end
