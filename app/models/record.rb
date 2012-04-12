9728c8cbf705ddc6dc2fd1cc7376602032665027
Antono Vasiljev <self@antono.info>
2012-04-13 00:37:51 +0300

    attr accessible

--- app/models/record.rb
+++ app/models/record.rb
@@ -38,7 +38,7 @@ class Record
   attr_accessible :title, :description, :typescript,
                   :timing, :tags, :columns, :rows,
                   :term, :shell, :uname, :xdg_current_desktop,
-                  :private
+                  :private, :related_url
 
   before_create :set_license, :generate_access_key
   after_create  :increment_counters!
