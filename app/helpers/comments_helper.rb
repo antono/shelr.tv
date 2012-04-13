ad492a6f182c386d172581f4e8e1821b28e0a47a
Antono Vasiljev <self@antono.info>
2012-04-13 14:14:11 +0300

    "comment#{comment.id}" -> dom_id(comment)

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,5 @@
 module CommentsHelper
- def comment_link(comment, text)
-   link_to(text, record_path(comment.commentable, anchor: "comment#{comment.id}"))
- end
+  def comment_link(comment, text)
+    link_to(text, record_path(comment.commentable, anchor: dom_id(comment)))
+  end
 end
