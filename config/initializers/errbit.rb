dc939551c11562b1681718e89eb4bfc5cc4e1db9
Antono Vasiljev <self@antono.info>
2012-04-22 22:58:48 +0300

    Disable errbit in test env

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,8 @@
-Airbrake.configure do |config|
-  config.api_key     = '23749797fdf7653d1621ddbad6a76dbd'
-  config.host        = 'errbit.shelr.tv'
-  config.port        = 80
-  config.secure      = config.port == 443
+unless Rails.env.test?
+  Airbrake.configure do |config|
+    config.api_key     = '23749797fdf7653d1621ddbad6a76dbd'
+    config.host        = 'errbit.shelr.tv'
+    config.port        = 80
+    config.secure      = config.port == 443
+  end
 end
