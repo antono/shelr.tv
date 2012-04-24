1c22ce19b374ce6a688968308a70bd115bc0de7c
Vasiliy Ermolovich <younash@gmail.com>
2012-04-24 10:57:01 +0300

    decorate comments

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,4 +1,26 @@
 class CommentDecorator < ApplicationDecorator
   decorates :comment
   decorates_association :user
+
+  def body
+    h.link_to(h.sanitize(model.body), record_path)
+  end
+
+  def updated_at
+    h.link_to(h.l(model.updated_at, format: :short), record_path)
+  end
+
+  def nickname
+    h.link_to(model.user.nickname, model.user)
+  end
+
+  def avatar
+    h.image_tag(self.user.avatar_url(50))
+  end
+
+  private
+
+  def record_path
+    h.record_path(model.commentable, anchor: h.dom_id(model))
+  end
 end
