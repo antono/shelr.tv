29881bb680d07ca19a0eb00e5e56efa7a3ca46dc
Vasiliy Ermolovich <younash@gmail.com>
2012-04-26 21:31:02 +0300

    remove useless comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,32 +15,24 @@ class HomeController < ApplicationController
   end
 
   def dashboard
-    # format atom
     if request.format == 'atom'
-      # key present => find user by key
       if params[:key].present?
         @user = User.where(atom_key: params[:key]).first
 
-        # user with given key found => render atom bulilder
         if @user.present?
           @comments = CommentDecorator.decorate(@user.comments_for_records.reverse)
-        # user with given key not found => render blank body
         else
           render text: ''
         end
-      # no key => render blank body
       else
         render text: ''
       end
-    # format html
     else
-      # user logged in => render fetch comments, render dashboard
       if logged_in?
         page      = params[:page] || 1
         comments  = current_user.comments_for_records
         comments  = Kaminari.paginate_array(comments).page(page).per(20)
         @comments = CommentDecorator.decorate(comments)
-      # user not logged in => redirect to records
       else
         redirect_to records_path
       end
