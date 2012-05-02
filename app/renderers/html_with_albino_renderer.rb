a0359d97bdbe0dc2956935d385b1fbdd530487e2
Antono Vasiljev <self@antono.info>
2012-05-02 20:13:29 +0300

    Default value for renderer

--- app/renderers/html_with_albino_renderer.rb
+++ app/renderers/html_with_albino_renderer.rb
@@ -1,5 +1,5 @@
 class HTMLWithAlbinoRenderer < Redcarpet::Render::HTML
   def block_code(code, language)
-    Albino.colorize(code, language)
+    Albino.colorize(code, language || :text)
   end
 end
