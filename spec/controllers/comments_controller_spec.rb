fbf6885265bcb982b775370dfae214a2aba843de
Antono Vasiljev <self@antono.info>
2012-07-14 14:04:44 +0300

    Fixed spec for CommentsController

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,59 +1,56 @@
 require 'spec_helper'
 
 describe CommentsController do
-  before do
-    @scope = mock('comments scope')
-    controller.stub(:commentable => @scope)
-  end
-
 
   describe "on GET index" do
     before do
+      @scope = mock('comments scope')
       @scope.stub(:comments => @scope)
       @scope.stub(:page => @scope)
       @scope.stub(:desc => @scope)
+
+      controller.stub(:commentable => @scope)
     end
+
+    let(:record)  { create :record }
 
     it "paginates comments given params[page]" do
       @scope.should_receive(:page).with("1")
-      get :index, page: 1
+      get :index, record_id: record.id.to_s, page: 1
     end
 
     it "sorts comments by created_at" do
       @scope.should_receive(:desc).with(:created_at)
-      get :index
+      get :index, record_id: record.id.to_s
     end
   end
 
   describe "POST create" do
-    let(:user) { create :user }
-    let(:commentable) { create :record }
-    let(:comment) { create :comment }
+    let!(:user)    { create :user }
+    let!(:record)  { create :record  }
 
     before do
-      controller.stub(:commentable => commentable)
       controller.stub(:current_user => user)
     end
 
     it "creates new comment for given commentable" do
-      commentable.comments.should_receive(:build)
-        .with({ "body" => 'hello world' })
-        .and_return(comment)
-      post :create, { comment: { body: 'hello world' } }
+      expect {
+        post :create, record_id: record.id.to_s, comment: { body: 'hello world' }
+      }.to change(record.comments, :count).by 1
     end
 
     it "creates assigns comment to current user" do
-      post :create, { comment: { body: 'hello world' } }
-      commentable.comments.first.user.should == user
+      post :create, record_id: record.id.to_s, comment: { body: 'hello world' }
+      record.comments.first.user.should == user
     end
 
     it "assigns new comment as @comment" do
-      post :create, { comment: { body: 'hello world' } }
+      post :create, record_id: record.id.to_s, comment: { body: 'hello world' }
       assigns(:comment).should_not be_blank
     end
 
     it "renders @comment as json" do
-      post :create, { comment: { body: 'hello world' } }
+      post :create, record_id: record.id.to_s, comment: { body: 'hello world' }
       lambda { JSON.parse(response.body) }.should_not raise_error
       JSON.parse(response.body)['body'].should_not be_blank
     end
